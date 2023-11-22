55e44a036e8d256207da380d17f722d157a422f5
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-11-22 08:41:15 +0500

    rectangle

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,24 @@
 from polygon import Polygon
 
 class Rectangle(Polygon):
-    pass
+    def __init__(self, height, width) -> None:
+        self.height = height
+        self.width = width
+    def get_Proof(self):
+        if (self.height<0 or self.width>0) and (self.height>0 or self.width<0) :
+            return True
+        else:
+            return False
+
+    def get_Array(self)->int:
+        S = self.height*self.width
+
+        return S
+    def get_Perimeter(self)->int:
+        
+        P = 2*(self.height+self.width)
+
+        return P
+    
+sum = Rectangle(-4,4)
+print(sum.get_Proof())
