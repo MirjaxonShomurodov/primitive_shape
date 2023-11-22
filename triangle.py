ac186d3562a7c608327bf00a012b7e531c347c13
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-11-22 08:43:00 +0500

    triangle

--- triangle.py
+++ triangle.py
@@ -1,7 +1,26 @@
 from polygon import Polygon
-
+from math import*
 class Triangle(Polygon):
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+    def get_proot(self)->int:
+        if self.a+self.b>self.c or self.a+self.c>self.b or self.b+self.c>self.a:
+            return True
+        else:
+            return False
+    def get_arr(self):
+
+        p = (self.a+self.b+self.c)/2
+
+        S = sqrt(p*(self.a+p)*(self.b+p)*(self.c+p)) 
+
+        return S
+    def get_permetr(self):
+
+        P = self.a+self.b+self.c
+
+        return P
+triangle = Triangle(2,3,4)
+print(triangle.get_arr())
