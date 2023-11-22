88053139362e8d74cc6bcc9fcccf92c58a66c4fd
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-11-22 08:41:55 +0500

    square

--- square.py
+++ square.py
@@ -1,4 +1,26 @@
 from polygon import Polygon
 
 class Square(Polygon):
-    pass
+    def __init__(self, height, width) -> None:
+        super().__init__(height, width)
+    
+    def get_proof(self)->int:
+
+        if (self.height<0 or self.width>0) and (self.height>0 or self.width<0) and (self.height==self.width):
+            return True
+        else:
+            return False
+
+
+    def get_arrey(self)->int:
+
+        S=self.height*self.width
+
+        return S
+    
+    def get_Perimeter(self,height:int)->int:
+        super().__init__(height,height)
+
+        return 4*self.height
+sum = Square(5,3)
+print(sum.get_proof())
